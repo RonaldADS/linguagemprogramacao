89685297d37a8500ddc9f52fb52c9e7e9ac8ab6c
Ronald <ronalddsbf@gmail.com>
2025-02-25 08:57:45 -0300

    meu primeiro programaweb

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 function seguir(){
     alert ('você agora está seguindo Ronald Santos')
 }
+//exercício 1
+function exe1(){
+    // vamos usar DOM para recuperar os dados do HTML
+    // DOM= Document Object Model
+    let nro1 = Number( document.getElementById("nro1").value)
+    let nro2 = Number(document.getElementById("nro2").value)
+
+    let sub = nro1 - nro2
+    document.getElementById("sub").innerText = "o resultado é" +sub
+
+}
