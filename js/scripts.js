bcc4cd3944136bfdda25dde8f3034f91518cac77
Ronald <ronalddsbf@gmail.com>
2025-03-18 08:00:43 -0300

    meu primeiro programaweb

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,34 @@
-function seguir(){
-    alert(`Você agora está seguindo Daniel Pires`)
-}
-// exercício 1
 function exe1(){
-    // vamos usar DOM para recuperar os dados do HTML
-    // DOM - Document Object Model
-    // converte texto para número - Number()
-    let nro1 = Number(document.getElementById("nro1").value)
-    let nro2 = Number(document.getElementById("nro2").value)
-    // vamos fazer a subtração
-    let sub = nro1 - nro2
-    // vamos mostrar o resultado ao usuário
-    //alert("A subtração é " + sub)
-    document.getElementById("sub").innerText = "O resultado é " + sub
+    //rec dados do usoário 
+    let nota1 = Number(document.getElementById("nota1").value)
+    let nota2 = Number(document.getElementById("nota2").value)
+    let nota3 = Number(document.getElementById("nota3").value)
+    let nota4 = Number(document.getElementById("nota4").value)
+    // calcular média
+    let media = (nota1 + nota2 + nota3 + nota4)/ 4
+    if (media >=7){
+        document.getElementById("media").innerText = "aprovado com média " + media
+    }
+    else {
+        document.getElementById("media").innerText = "aprovado com média " + media
+    }
 }
-
 function exe2(){
-    let nro1 = Number(document.getElementById("nro1").value)
-    let nro2 = Number(document.getElementById("nro2").value)
-    let nro3 = Number(document.getElementById("nro3").value)
-    let multiplicacao = nro1 * nro2 * nro3
-    document.getElementById("multiplicacao").innerText = "O resultado é " + multiplicacao
-}
-
-function exe3(){
-    let nro1 = Number(document.getElementById("nro1").value)
-    let nro2 = Number(document.getElementById("nro2").value)
-    let divisao = nro1 / nro2
-    document.getElementById("divisao").innerText = "O resulta é " + divisao
-}
-
-function exe4(){
-    let nro1 = Number(document.getElementById("nro1").value)
-    let nro2 = Number(document.getElementById("nro2").value)
-    // média ponderada
-    let mp = ((nro1 * 2) + (nro2 * 3)) / 5
-    document.getElementById("mp").innerText = "O resultado é " + mp
-}
-function exe5(){
-    let preco = Number(document.getElementById("preco").value)
-    let desconto = (preco * 10) / 100
-    document.getElementById("desconto").innerText = "O novo preço é de " 
-    + (preco - desconto)
+    let nota1 = Number(document.getElementById("nota1").value)
+    let nota2 = Number(document.getElementById("nota2").value)
+    let nota3 = Number(document.getElementById("nota3").value)
+    let nota4 = Number(document.getElementById("nota4").value)
+    let media = (nota1+nota2)/2
+    if ((media>=0) && (media>3)){
+        document.getElementById("media").innerHTML = "reprovado" + media
+    }
+    else if ((media>=3) && (media>7)){
+        document.getElementById("media").innerHTML = "exame" + media
+    }
+    else if ((media>=7) && (media>10)){
+        document.getElementById("media").innerHTML = "aprovado" + media
+    }
+    else {
+            document.getElementById("media").innerHTML = "problema com notas" + media
+    }
 }
