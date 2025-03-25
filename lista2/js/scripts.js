026cfa63485502209b9a711ceaa13b4da14d6382
Ronald <ronalddsbf@gmail.com>
2025-03-25 08:38:55 -0300

    meu primeiro programaweb

--- lista2/js/scripts.js
+++ lista2/js/scripts.js
@@ -1,45 +1,157 @@
-function seguir(){
-    alert(`Você agora está seguindo Daniel Pires`)
-}
-// exercício 1
 function exe1(){
-    // vamos usar DOM para recuperar os dados do HTML
-    // DOM - Document Object Model
-    // converte texto para número - Number()
-    let nro1 = Number(document.getElementById("nro1").value)
-    let nro2 = Number(document.getElementById("nro2").value)
-    // vamos fazer a subtração
-    let sub = nro1 - nro2
-    // vamos mostrar o resultado ao usuário
-    //alert("A subtração é " + sub)
-    document.getElementById("sub").innerText = "O resultado é " + sub
+    //rec dados do usoário 
+    let nota1 = Number(document.getElementById("nota1").value)
+    let nota2 = Number(document.getElementById("nota2").value)
+    let nota3 = Number(document.getElementById("nota3").value)
+    let nota4 = Number(document.getElementById("nota4").value)
+    // calcular média
+    let media = (nota1 + nota2 + nota3 + nota4)/ 4
+    if (media >=7){
+        document.getElementById("media").innerText = "aprovado com média " + media
+    }
+    else {
+        document.getElementById("media").innerText = "aprovado com média " + media
+    }
 }
-
 function exe2(){
-    let nro1 = Number(document.getElementById("nro1").value)
-    let nro2 = Number(document.getElementById("nro2").value)
-    let nro3 = Number(document.getElementById("nro3").value)
-    let multiplicacao = nro1 * nro2 * nro3
-    document.getElementById("multiplicacao").innerText = "O resultado é " + multiplicacao
+    let nota1 = Number(document.getElementById("nota1").value)
+    let nota2 = Number(document.getElementById("nota2").value)
+    let nota3 = Number(document.getElementById("nota3").value)
+    let nota4 = Number(document.getElementById("nota4").value)
+    let media = (nota1+nota2)/2
+    if ((media>=0) && (media>3)){
+        document.getElementById("media").innerHTML = "reprovado" + media
+    }
+    else if ((media>=3) && (media>7)){
+        document.getElementById("media").innerHTML = "exame" + media
+    }
+    else if ((media>=7) && (media>10)){
+        document.getElementById("media").innerHTML = "aprovado" + media
+    }
+    else {
+            document.getElementById("media").innerHTML = "problema com notas" + media
+    }
 }
-
 function exe3(){
     let nro1 = Number(document.getElementById("nro1").value)
     let nro2 = Number(document.getElementById("nro2").value)
-    let divisao = nro1 / nro2
-    document.getElementById("divisao").innerText = "O resulta é " + divisao
+    let menor
+    if (nro1<nro2){
+        menor = nro1
+    }
+    if (nro1>nro2){
+        menor = nro2
+    }
+    else{
+        menor= "os dois números são iguais"
+    }
+    document.getElementById("menor").innerHTML = "o menor é " + menor
 }
-
 function exe4(){
     let nro1 = Number(document.getElementById("nro1").value)
     let nro2 = Number(document.getElementById("nro2").value)
-    // média ponderada
-    let mp = ((nro1 * 2) + (nro2 * 3)) / 5
-    document.getElementById("mp").innerText = "O resultado é " + mp
+    let nro3 = Number(document.getElementById("nro3").value)
+    let maior 
+    if (nro1 >= nro2 && nro1 >= nro3){
+        document.getElementById("maior").innerHTML = "O maior é " + nro1
+    }
+    else if (nro2 >= nro1 && nro2 >= nro3){
+        document.getElementById("maior").innerHTML = "O maior é " + nro2
+    }
+    else if (nro3 >= nro1 && nro3 >= nro2){
+        document.getElementById("maior").innerHTML = "O maior é " + nro3
+    }
 }
 function exe5(){
-    let preco = Number(document.getElementById("preco").value)
-    let desconto = (preco * 10) / 100
-    document.getElementById("desconto").innerText = "O novo preço é de " 
-    + (preco - desconto)
+    // recupera os dados do usuário
+    let nro1 = Number(document.getElementById("nro1").value)
+    let nro2 = Number(document.getElementById("nro2").value)
+    let selecao = Number(document.getElementById("selecao").value)
+    // utilização do escolha
+    switch (selecao){
+        case 1: document.getElementById("result").innerText = (nro1 + nro2) / 2
+                break
+        case 2: if (nro1 >= nro2){
+                    document.getElementById("result").innerText = nro1 - nro2
+                }
+                else {
+                    document.getElementById("result").innerText = nro2 - nro1
+                }
+                break
+        case 3: document.getElementById("result").innerText = nro1 * nro2
+                break
+        case 4: if (nro2 != 0){
+                    document.getElementById("result").innerText = nro1 / nro2
+                }
+                else {
+                    document.getElementById("result").innerText = "Divisão por zero"
+                }
+    }
+}
+function exe6 (){
+    //recupera dados do usoario
+    let nro1 = Number(document.getElementById("nro1").value)
+    let nro2 = Number(document.getElementById("nro2").value)
+    let selecao = Number(document.getElementById("selecao").value)
+    switch(selecao){
+        case 1:document.getElementById("result").innerHTML = "a potência é " + 
+            Math.pow(nro1,nro2)
+            break
+        case 2:document.getElementById("result").innerHTML = "rq do nro 1" + Math.sqrt(nro1).toFixed(2) + 
+            "<br/> rq do  nro 2" + Math.sqrt(nro2).toFixed(2)
+        break
+        case 3:document.getElementById("result").innerHTML = "rq do nro 1" + Math.cbrt(nro1).toFixed(2) + 
+                "<br/> rq do nro 2" + Math.sqrt(nro2).toFixed(2)
+    }
+}
+function exe7(){
+    //rec dados do usoário
+    let salario = number (document.getElementById("salário").value)
+    //verif valor do salário 
+    if (salario<0){
+    document.getElementById("novo").innerHTML="salario negativo"
+    }
+    else if (salario < 500){
+        document.getElementById("novo").innerHTML = "novo salário" + + (salario + salario*30/100)
+    }
+    else {
+        document.getElementById("novo").innerHTML = "salario maior ou igual a 500, sem reajuste"
+    }
+}
+function exe8(){
+    //ttemplate string
+    
+    let salario = number (document.getElementById("salário").value)
+    if (salario<0){
+    document.getElementById("novo").innerHTML="salario negativo"
+    }
+    else if (salario < 500){
+    document.getElementById("novo").innerHTML = 'novo salário $(salario) + {salario*35/100}'
+    }
+    else {
+    document.getElementById("novo").innerHTML = "novo salário"  + (salario + salario*15/100)
+    }
+}
+function exe9(){
+    //saldo
+    let saldo = Number (document.getElementById(saldo).value)
+    //credito
+    let credito =0//começa com valor zero
+    if (saldo >400){
+        credito = (saldo*30/100)
+    }
+    else if ((saldo>=300) && (saldo <=400)){
+    credito = (saldo *25/100)
+    }
+    else if ((saldo>=200) && (saldo <=300)){
+        credito = (saldo *20/100)
+    }
+    else if ((saldo>=0) && (saldo <=200)){
+        credito = (saldo *10/100)
+    }
+    else{// saldo negativo 
+        document.getElementById("credito").innerHTML = "saldo não pode ser negativo"
+        return//saiu da função 
+    }
+    document.getElementById("credito").innerHTML = 'saldo ${saldo} e crédito ${credito}'
 }
